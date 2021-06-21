ad4a85095aeb17fff935982225be2b1022ffcfb4
Marcello Seri <marcello.seri@gmail.com>
2021-06-21 10:23:20 +0200

    Add forced lagrangian integrator for comparison
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>

---
+++ integrators/lagrangian.py
@@ -0,0 +1,30 @@
+import numpy as np
+from scipy.optimize import fsolve
+
+
+def forced_lagrangian(system, tspan, p0, q0):
+    """
+    Integrate [system] with initial conditions [p0], [q0]
+    using a second order forced lagrangian integrator.
+
+    [tspan] is usually [np.linspace(t0, tfinal, num=steps)]
+    """
+    dt = tspan[1] - tspan[0]
+    steps = len(tspan)
+    init = [p0, q0]
+
+    solpq = np.empty([steps, *np.shape(init)], dtype=np.float64)
+    solpq[0] = np.array(init)
+
+    for i in range(steps - 1):
+        p, q = np.copy(solpq[i])
+
+        qnew = fsolve(
+            lambda qnew: p + system.d1l(q, qnew, dt) + system.fm(q, qnew, dt),
+            q + dt * p,
+        )
+        pnew = system.d2l(q, qnew, dt) + system.fp(q, qnew, dt)
+
+        solpq[i + 1] = [pnew, qnew]
+
+    return solpq, tspan
